85bdc6969744c43d52fa33107d3b82c111530685
Rami <ramiahmedportland@gmail.com>
2020-02-27 10:00:19 -0800

    add the conditional statment in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,24 @@
   var food = parseInt($("#food").val());
   var camping = parseInt($("#camping").val());
   var trveling = parseInt($("#traveling").val());
-  var Movies = parseInt($("#movies").val());
+  var movies = parseInt($("#movies").val());
+  var total = beech + food + camping + trveling + movies;
 
+  if (total < 5) {
+    alert("Please complete the survey questions");
+  } else if (total === 5) {
+    $(".result").text("JavaScript");
+  } else if (total === 7 || total >= 9) {
+    $(".result").text("Ruby");
+  } else if (total === 6 || total === 8) {
+    $(".result").text("Python");
+  } else {
+    $(".result").text("Please try one more time");
+  }
+
+    $("#result").show();
  });
+
+
+
+
