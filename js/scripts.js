b09581b5a8ce3abbe82f4bbb69a4ceda1a595afc
Rami <ramiahmedportland@gmail.com>
2020-02-27 13:11:36 -0800

    finish the logic for the form and debug in order to get it to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
- $(document).ready(function() {
-  event.preventDefault();
-  var beech = parseInt($("#beech").val());
-  var food = parseInt($("#food").val());
-  var camping = parseInt($("#camping").val());
-  var trveling = parseInt($("#traveling").val());
-  var movies = parseInt($("#movies").val());
-  var total = beech + food + camping + trveling + movies;
+$(document).ready(function() {
+  $("form").submit(function(event) {
+      event.preventDefault();
+      var beech = parseInt($("#beech").val());
+      var food = parseInt($("#food").val());
+      var camping = parseInt($("#camping").val());
+      var travel = parseInt($("#travel").val());
+      var movie = parseInt($("#movie").val());
+      var total = beech + food + camping + travel + movie;
 
-  if (total < 5) {
-    alert("Please complete the survey questions");
-  } else if (total === 5) {
-    $(".result").text("JavaScript");
-  } else if (total === 7 || total >= 9) {
-    $(".result").text("Ruby");
-  } else if (total === 6 || total === 8) {
-    $(".result").text("Python");
-  } else {
-    $(".result").text("Please try one more time");
-  }
+      if (total < 5) {
+        alert("Please complete the survey questions");
+      } else if (total === 5) {
+        $(".result").text("JavaScript");
+      } else if (total === 7 || total >= 9)  {
+        $(".result").text("Ruby"); 
+      } else if (total === 6  || total === 8) {
+       $(".result").text("Python");
+     } else{
+      $(".result").text("Please try one more time");
+     }
 
-    $("#result").show();
- });
+      $("#result").show();
+});
+});
 
 
 
