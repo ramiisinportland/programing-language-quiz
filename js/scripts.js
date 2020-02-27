38afae7ffb63977a31d702134e9472c4071fd700
Rami <ramiahmedportland@gmail.com>
2020-02-27 04:22:55 -0800

    declear variables

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ $(document).ready(function() {
+  event.preventDefault();
+  var beech = parseInt($("#beech").val());
+  var food = parseInt($("#food").val());
+  var camping = parseInt($("#camping").val());
+  var trveling = parseInt($("#traveling").val());
+  var Movies = parseInt($("#movies").val());
+
+ });
