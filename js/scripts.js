eabb23e5489a26fe630a337b0f40620516ffed23
Rami <ramiahmedportland@gmail.com>
2020-02-27 16:59:58 -0800

    fixed the indentation for all and cleaned up the codes and the spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
-$(document).ready(function() {
-  $("form").submit(function(event) {
-      event.preventDefault();
-      var beech = parseInt($("#beech").val());
-      var food = parseInt($("#food").val());
-      var camping = parseInt($("#camping").val());
-      var travel = parseInt($("#travel").val());
-      var movie = parseInt($("#movie").val());
-      var total = beech + food + camping + travel + movie;
+$(document).ready(function () {
+	$("form").submit(function (event) {
+		event.preventDefault();
+		var beech = parseInt($("#beech").val());
+		var food = parseInt($("#food").val());
+		var camping = parseInt($("#camping").val());
+		var travel = parseInt($("#travel").val());
+		var movie = parseInt($("#movie").val());
+		var total = beech + food + camping + travel + movie;
 
-      if (total < 5) {
-        alert("Please complete the survey questions");
-      } else if (total === 5) {
-        $(".result").text("JavaScript");
-      } else if (total === 7 || total >= 9)  {
-        $(".result").text("Ruby"); 
-      } else if (total === 6  || total === 8) {
-       $(".result").text("Python");
-     } else{
-      $(".result").text("Please try one more time");
-     }
+		if (total < 5) {
+			alert("Please complete the survey questions");
+		}
+		else if (total === 5) {
+			$(".result").text("JavaScript");
+		}
+		else if (total === 7 || total >= 9) {
+			$(".result").text("Ruby");
+		}
+		else if (total === 6 || total === 8) {
+			$(".result").text("Python");
+		}
+		else {
+			$(".result").text("Please try one more time");
+		}
 
-      $("#result").show();
-});
+		$("#result").show();
+	});
 });
 
 
